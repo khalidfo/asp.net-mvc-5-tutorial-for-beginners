22e1b4b7f193a3a82b0081cc76ffb9f672b5987f
Khalid Farhan <khalidfo@gmail.com>
2022-06-11 21:49:26 +0600

    Class 02

---
+++ MvcViewBag/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using MvcViewBagData.HR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcViewBag.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from ViewBag";
+            ViewBag.CurrentDateTime = DateTime.Now;
+
+            string[] vegetables = { "Carrot", "Broccoli", "Asparagus", "Cucumber", "Eggplant" };
+            ViewBag.Vegetables = vegetables;
+
+
+            ViewBag.Countries = new List<string>
+            {
+                "Bangladesh",
+                "Japan",
+                "Saudi Arabia",
+                "Egypt",
+                "Malaysia"
+            };
+
+            Employee employee = new Employee();
+            employee.Id = 1;
+            employee.EmpName = "Ali";
+            employee.EmpAddress = "Mirpur Dhaka";
+            employee.Dob = new DateTime(2001, 6, 25);
+
+            ViewBag.AliDetails = employee;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcViewBagData/HR/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvcViewBagData.HR
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public DateTime Dob { get; set; }
+    }
+}
